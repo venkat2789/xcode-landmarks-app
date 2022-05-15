bd763de75f101c2c07bbc48f550e585ee7d36d46
venkat2789 <venkatachalapathy.27@gmail.com>
2022-05-15 10:39:15 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Venkatachalapathy Sivaprakasam on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
