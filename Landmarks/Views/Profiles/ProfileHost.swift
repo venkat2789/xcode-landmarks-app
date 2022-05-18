070384c97ef4b1e6fecd6fdaaf7dd5b0e065b1e0
venkat2789 <venkatachalapathy.27@gmail.com>
2022-05-17 20:22:22 -0400

    Added Profile section and added it as Toolbar, accessible as sheet

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Venkatachalapathy Sivaprakasam on 5/17/22.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
